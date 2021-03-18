bb272c1ced4b9354b29a264d246174a858cd1e3b
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-03-18 18:15:14 +0100

    Add check to detect variables starting with `vault_`

--- check.py
+++ check.py
@@ -29,14 +29,18 @@ class Context:
 
     def error(self, path: str, msg: str):
         self.errored = True
-        print(f'ERROR in "{path}": {msg}', file=sys.stderr)
+        print(f'ERROR in "{path}" => {msg}', file=sys.stderr)
 
+    def line_error(self, path: str, line: int, msg: str):
+        self.errored = True
+        print(f'ERROR in {path}:{line} => {msg}', file=sys.stderr)
 
 
 # ----- Actual checks trying to detect secrets -------------------------------
 
 def check_file(ctx: Context, content: bytes, path: str):
     check_vault_filename(ctx, content, path)
+    check_var_starting_with_vault(ctx, content, path)
 
 
 def check_vault_filename(ctx: Context, content: bytes, path: str):
@@ -45,6 +49,12 @@ def check_vault_filename(ctx: Context, content: bytes, path: str):
     """
     if os.path.basename(path) == "vault" and not content.startswith(b"$ANSIBLE_VAULT"):
         ctx.error(path, f'has filename "vault" but does not start with "$ANSIBLE_VAULT"')
+
+def check_var_starting_with_vault(ctx: Context, content: bytes, path: str):
+    for lineno, line in enumerate(content.splitlines(), start=1):
+        if line.startswith(b"vault_") and b":" in line:
+            linestr = line.decode(errors="replace")
+            ctx.line_error(path, lineno, f'looks like a vault variable definition: {linestr}')
 
 
 
