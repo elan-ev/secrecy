ef24c31ba098538e44b96c0f321c4d7085a27a7e
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-03-18 17:49:59 +0100

    Add Context to improve error tracking and output

--- check.py
+++ check.py
@@ -6,21 +6,45 @@ import sys
 
 
 def main():
+    ctx = Context()
     path = sys.argv[1]
     with open(path, "rb") as f:
         content = f.read()
-        check_file(content, path)
-    pass
+        check_file(ctx, content, path)
 
-def eprint(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
+    if ctx.errored:
+        eprint("")
+        eprint("Potentially found unencrypted secrets!")
+        sys.exit(1)
 
-def check_file(content: bytes, path: str):
-    check_vault_filename(content, path)
+# ----- Utilities ------------------------------------------------------------
 
-def check_vault_filename(content: bytes, path: str):
+def eprint(msg: str):
+    print(msg, file=sys.stderr)
+
+class Context:
+    """Utility type to keep track of error state and emit errors"""
+
+    errored = False
+
+    def error(self, path: str, msg: str):
+        self.errored = True
+        print(f'ERROR in "{path}": {msg}', file=sys.stderr)
+
+
+
+# ----- Actual checks trying to detect secrets -------------------------------
+
+def check_file(ctx: Context, content: bytes, path: str):
+    check_vault_filename(ctx, content, path)
+
+
+def check_vault_filename(ctx: Context, content: bytes, path: str):
+    """Emits an error if the filename is "vault" but the file does not start
+    with "$ANSIBLE_VAULT"
+    """
     if os.path.basename(path) == "vault" and not content.startswith(b"$ANSIBLE_VAULT"):
-        eprint(f'Error: {path} has filename "vault" but does not start with "$ANSIBLE_VAULT"')
+        ctx.error(path, f'has filename "vault" but does not start with "$ANSIBLE_VAULT"')
 
 
 
