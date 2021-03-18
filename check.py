f5887ba8763a69adce2c82df92662a013a400e90
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-03-18 18:59:54 +0100

    Add recursive directory checking

--- check.py
+++ check.py
@@ -6,19 +6,12 @@ import re
 import sys
 
 
-def main():
-    ctx = Context()
-    path = sys.argv[1]
-    with open(path, "rb") as f:
-        content = f.read()
-        check_file(ctx, content, path)
-
-    if ctx.errored:
-        eprint("")
-        eprint("Potentially found unencrypted secrets!")
-        sys.exit(1)
 
 # ----- Utilities ------------------------------------------------------------
+
+def read_file(path: str) -> bytes:
+    with open(path, "rb") as f:
+        return f.read()
 
 def eprint(msg: str):
     print(msg, file=sys.stderr)
@@ -30,7 +23,7 @@ class Context:
 
     def error(self, path: str, msg: str):
         self.errored = True
-        print(f'ERROR in "{path}" => {msg}', file=sys.stderr)
+        print(f'ERROR in {path} => {msg}', file=sys.stderr)
 
     def line_error(self, path: str, line: int, msg: str):
         self.errored = True
@@ -59,11 +52,39 @@ def check_var_starting_with_vault(ctx: Context, content: bytes, path: str):
             ctx.line_error(path, lineno, f'looks like a vault variable definition: {linestr}')
 
 def check_private_key(ctx: Context, content: bytes, path: str):
-    pattern = re.compile(b"-----BEGIN [A-Z]+ PRIVATE KEY-----")
+    pattern = re.compile(b"-----BEGIN .+PRIVATE KEY-----")
     for lineno, line in enumerate(content.splitlines(), start=1):
         if pattern.search(line) is not None:
             linestr = line.decode(errors="replace")
             ctx.line_error(path, lineno, f'unencrypted private key: {linestr}')
+
+
+# ----- Main: entry points ---------------------------------------------------
+
+def main():
+    ctx = Context()
+    path = sys.argv[1]
+
+    check_current(ctx, path)
+
+    if ctx.errored:
+        eprint("")
+        eprint("Potentially found unencrypted secrets!")
+        sys.exit(1)
+
+def check_current(ctx: Context, path: str):
+    """Checks all files in 'path' in their current version (not using git)"""
+
+    if os.path.isfile(path):
+        content = read_file(path)
+        check_file(ctx, content, path)
+    else:
+        for dirpath, dirnames, filenames in os.walk(path):
+            for filename in filenames:
+                fullpath = os.path.join(dirpath, filename);
+                content = read_file(fullpath)
+                check_file(ctx, content, fullpath)
+
 
 
 
